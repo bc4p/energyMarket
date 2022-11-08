b1bf64b002f94d728e88855f5efdd93e1cb9606d
pietro99 <pietro.piccini@hotmail.com>
2022-11-08 18:03:52 +0100

    redis event listeners

---
+++ redisConnector.py
@@ -0,0 +1,32 @@
+import redis
+import b4p
+
+if not b4p.started():
+    b4p.init()
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+p = r.pubsub()
+
+
+import b4p
+import json
+def handle_registration(data):
+    data = json.loads(data.decode())
+    #print(data)
+    #print(data["transaction_id"])
+    b4p.Accounts.new(data["transaction_id"])
+def handle_account_registration(data):
+    data = json.loads(data.decode())
+    #print(data)
+    #print(f"registre account {data}")
+
+while True:
+    p.psubscribe('*/register_participant')
+    p.psubscribe('*/response/register_participant')
+
+    for i in p.listen():
+        #print(i)
+        patter = i["pattern"]
+        if patter == b'*/register_participant':
+            handle_account_registration(i["data"])    
+        if patter == b'*/response/register_participant':
+            handle_registration(i["data"]) 
