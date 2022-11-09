1193330375a8acd3af31c6872a240178e49d902f
Florian Maurer <maurer@fh-aachen.de>
2022-11-09 10:13:56 +0100

    move psubscribe to the first uncomment b4p

--- redisConnector.py
+++ redisConnector.py
@@ -1,28 +1,27 @@
 import redis
-import b4p
+import json
+#import b4p
 
-if not b4p.started():
-    b4p.init()
+#if not b4p.started():
+#    b4p.init()
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 p = r.pubsub()
+p.psubscribe('*/register_participant')
+p.psubscribe('*/response/register_participant')
 
 
-import b4p
-import json
+
 def handle_registration(data):
     data = json.loads(data.decode())
     #print(data)
-    #print(data["transaction_id"])
-    b4p.Accounts.new(data["transaction_id"])
+    print(data["transaction_id"])
+    #b4p.Accounts.new(data["transaction_id"])
 def handle_account_registration(data):
     data = json.loads(data.decode())
     #print(data)
     #print(f"registre account {data}")
 
 while True:
-    p.psubscribe('*/register_participant')
-    p.psubscribe('*/response/register_participant')
-
     for i in p.listen():
         #print(i)
         patter = i["pattern"]
